7ba2e7d635ec32b3806c299f6289ca4f6d48d29d
Maxime Legare <mleg1234@protonmail.com>
2023-08-31 14:55:59 -0400

    add picture of robot to home page via an object

--- Alchemy/Controllers/HomeController.cs
+++ Alchemy/Controllers/HomeController.cs
@@ -4,9 +4,35 @@ using Alchemy.Models;
 
 namespace Alchemy.Controllers;
 
+public class Robot
+    {
+        private string name;
+        private string url;
+        private double height;
+        private double weight;
+        private string country;
+
+        public string Name {get => name;}
+        public string Url {get => url;}
+        public double Height {get => height;}
+        public double Weight {get => weight;}
+        public string Country {get => country;}
+        public Robot(string name, string url, double height, double weight, string country){
+            this.name = name;
+            this.url = url;
+            this.height = height;
+            this.weight = weight;
+            this.country = country;
+        }
+    }
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+
+    
+
+
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -15,6 +41,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewData["robot"] = new Robot("bob", "https://robohash.org/bob", 5, 4, "tailand");
         return View();
     }
 
@@ -26,6 +53,8 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
     }
 }
