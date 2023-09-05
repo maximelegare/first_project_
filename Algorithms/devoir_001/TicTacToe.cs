115e2791f186838f066be7e32b19f80fe3b59539
Maxime Legare <mleg1234@protonmail.com>
2023-09-05 08:30:12 -0400

    add tik tak toe

---
+++ Algorithms/devoir_001/TicTacToe.cs
@@ -0,0 +1,20 @@
+namespace Algorithms.devoir_001;
+
+public class TicTacToe
+{
+    private readonly string x = "X";
+    private readonly string o = "O";
+
+    private enum Players
+    {
+        x,
+        o,
+    }
+    private enum ScareState
+    {
+        x,
+        o,
+        Null
+    }
+    
+}
