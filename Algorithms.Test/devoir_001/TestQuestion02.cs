d727e6d38e1fa9c7820bf753b16095fea2dc2cd3
Maxime Legare <mleg1234@protonmail.com>
2023-09-11 10:25:12 -0400

    staircase tests work

--- Algorithms.Test/devoir_001/TestQuestion02.cs
+++ Algorithms.Test/devoir_001/TestQuestion02.cs
@@ -1,25 +1,24 @@
 using Algorithms.devoir_001;
 using System;
+
 namespace Algorithms.Test;
 
 [TestClass]
 public class TestQuestion02
 {
-    [TestMethod]
-    public static void TestStaircase()
+    // [TestMethod]
+    public  bool CreateStaircaseForTesting(int height, int width)
     {
-        int height = 2;
-        int width = 3;
         string result = new Question02().CreateStaircase(height, width);
-        
+        bool ret = true;
         var sepResult = result.Split('\n');
 
         string expected = "";
         //Establish pattern for first line
-        for(int i=0; i<width; i++){
-            expected+= "#";
+        for (int i = 0; i < width; i++)
+        {
+            expected += "#";
         }
-
 
         for (int i = 0; i < 5; i++)
         {
@@ -27,13 +26,24 @@ public class TestQuestion02
             {
                 //Validate if ok
                 Console.WriteLine(expected);
-                Console.WriteLine(sepResult[i*height + j]);
-                Assert.IsTrue(sepResult[i*height + j].EndsWith(expected));
+                Console.WriteLine(sepResult[i * height + j]);
+                if (!sepResult[i * height + j].EndsWith(expected))
+                {
+                    ret = false;
+                }
             }
             //Change expected
-            for(int k=0; k<width; k++){
-               expected+= "#"; 
+            for (int k = 0; k < width; k++)
+            {
+                expected += "#";
             }
         }
+        return ret;
+    }
+
+    [TestMethod]
+    public  void TestStaircase()
+    {
+        Assert.AreEqual(true, CreateStaircaseForTesting(1, 2));
     }
 }
